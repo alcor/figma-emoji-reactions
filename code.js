876c46284c34008d0c6c20df44e05be4a11b3ac7
Nicholas Jitkoff <nicholas@jitkoff.com>
2020-11-16 23:49:24 -0800

    Very basic chat bubble creation

--- code.js
+++ code.js
@@ -1,1 +1,42 @@
-throw new Error("This plugin template uses Typescript. Follow the instructions in `README.md` to generate `code.js`.")
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+figma.showUI(__html__);
+figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+    yield figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+    if (msg.type === 'add-reaction') {
+        const frame = figma.createFrame();
+        frame.resizeWithoutConstraints(300, 150);
+        frame.name = "hello";
+        frame.x = figma.viewport.center.x - frame.width / 2;
+        frame.y = figma.viewport.center.y - frame.height / 2;
+        frame.layoutMode = "HORIZONTAL";
+        frame.horizontalPadding = 50;
+        frame.verticalPadding = 50;
+        frame.layoutAlign = "STRETCH";
+        frame.cornerRadius = frame.height / 2;
+        frame.bottomLeftRadius = 0;
+        const label = figma.createText();
+        label.layoutAlign = "STRETCH";
+        label.resizeWithoutConstraints(100, 100);
+        label.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
+        label.characters = msg.string || "❤️";
+        if (label.characters.length < 3)
+            label.fontSize = 24;
+        label.fontSize = 30;
+        label.textAlignHorizontal = 'CENTER';
+        label.textAlignVertical = 'CENTER';
+        label.textAutoResize = "WIDTH_AND_HEIGHT";
+        frame.appendChild(label);
+        figma.group([frame], figma.currentPage);
+        figma.viewport.scrollAndZoomIntoView([frame]);
+        figma.currentPage.selection = [label];
+    }
+    figma.closePlugin();
+});
--- code.ts
+++ code.ts
@@ -1,33 +1,36 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+figma.ui.onmessage = async (msg) => {
+  await figma.loadFontAsync({ family: "Roboto", style: "Regular" })
+  if (msg.type === 'add-reaction') {
+    const frame = figma.createFrame()
+    frame.resizeWithoutConstraints(300, 150)
+    frame.name = "hello" 
+    frame.x = figma.viewport.center.x - frame.width / 2
+    frame.y = figma.viewport.center.y - frame.height / 2
+    frame.layoutMode = "HORIZONTAL"
+    frame.horizontalPadding = 50
+    frame.verticalPadding = 50
+    frame.layoutAlign = "STRETCH"
+    frame.cornerRadius = frame.height / 2;
+    frame.bottomLeftRadius = 0;
+
+    const label = figma.createText()
+    label.layoutAlign = "STRETCH"
+    label.resizeWithoutConstraints(100, 100)
+    label.fills = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
+    label.characters = msg.string || "❤️"
+    if (label.characters.length < 3) label.fontSize = 24;
+    label.fontSize = 30
+    label.textAlignHorizontal = 'CENTER'
+    label.textAlignVertical = 'CENTER'
+    label.textAutoResize = "WIDTH_AND_HEIGHT"
+    frame.appendChild(label)
+
+    figma.group([frame], figma.currentPage)
+    figma.viewport.scrollAndZoomIntoView([frame]);
+    figma.currentPage.selection = [label];
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
   figma.closePlugin();
 };
