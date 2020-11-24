7c1359fe281664635e7cfd3a0bf206572cb72359
Nicholas Jitkoff <nicholas@jitkoff.com>
2020-11-23 18:50:00 -0800

    Resize emoji

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-figma.showUI(__html__, { width: 300, height: 260 });
+figma.showUI(__html__, { width: 300, height: 240 });
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         let settings = yield figma.clientStorage.getAsync("settings");
